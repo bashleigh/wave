aa8de3578ae38fbc34383a60d95c5737b01092ec
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-05-26 19:19:47 +0100

    feat: added human readable charge

--- src/components/fundimental.tsx
+++ src/components/fundimental.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Particle, StateType } from "wave-func";
+import { AbstractParticle } from "wave-func";
 import './fundimental.scss';
 
-export const Fundimental = ({particle, onClick}: {particle: Particle, onClick?: () => void}) => (
+export const Fundimental = ({particle, onClick}: {particle: AbstractParticle, onClick?: () => void}) => (
   <div className="card" onClick={() => onClick && onClick()}>
     <div className={`particle is-${particle.group.replace(' ', '-')} ${particle.type ? `is-${particle.type}` : ''} is-${particle.antiMatter ? 'antimatter' : 'matter'}`}>
       <h2 className="title">{particle.name}</h2>
       <div className="particle-symbol"><p>{particle.symbol}</p></div>
       <div className="particle-info">
         <div>Spin: {particle.spin}</div>
-        <div>Charge: {particle.charge.value !== 0 && particle.charge.state === StateType.positive ? '+' : '-'}{particle.charge.value}</div>
+        <div>Charge: {particle.chargeForHumans}</div>
         <div>Mass: {particle.mass}</div>
         <div>Applied forces: {particle.appliedForces.map(force => <span key={`${particle.name}-${force}`}>{force}</span>)}</div>
         {particle.scalar && <div>Scalar</div>}
